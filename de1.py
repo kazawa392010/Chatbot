a9d5590a3825aaea143f6f1740ee99f7638c4d2f
Kaza <kazawa3910@gmail.com>
2026-02-02 23:48:31 +0700

    Update de1.py

--- de1.py
+++ de1.py
@@ -4,12 +4,13 @@ import time
 
 
 #Cách 1 code sạch - gọn - dễ đọc
+#Học thuộc cú pháp thì học từ đây đến
 def c1(x):
   S = 0
   for i in range(3, x + 1, 2):
     S += i**0.5
   return S
-
+# đến đây nha học nhiêu đây đủ sống rồi ngắn lắm cái bên dưới ngắn hơn nhg hơi khó thuộc với sợ thầy bắt giải thích code á nên ní đừng sài! :>>
 
 #Cách 2 code sạch nhanh gọn cần import thư viện nha
 def c2(x):
@@ -29,3 +30,5 @@ end2 = time.time()
 print(f'Cách 2 kết quả là {c} và chạy xg trong {end2 - start2}')
 ss = "Cách 1 nhanh hơn cách 2" if (end - start) < (end2 - start2) else "Cách 2 nhanh hơn cách 1"
 print(ss)
+
+
